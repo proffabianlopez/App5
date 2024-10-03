16ee46e580e1ede6e66e9f64a3275252bc0eb5c8
Angel Gomez <angelezequielgomez15@gmail.com>
2024-10-03 10:48:50 -0300

    last changes

--- www/app/views/admin/dashboard.php
+++ www/app/views/admin/dashboard.php
@@ -81,15 +81,7 @@ if(empty($_SESSION)){
                                         </span>
                                         <h2 class="StepTitle">Administrar Doctores</h2>
                                         <p class="cl-effect-1">
-                                            <a href="manage-doctors.php">
-                                                <!-- <php
-                                                $result1 = mysqli_query($con, "SELECT * FROM doctors");
-                                                $num_rows1 = mysqli_num_rows($result1);
-                                                {
-                                                ?>
-                                                    Total Doctors: <php echo htmlentities($num_rows1); }
-                                                ?> -->
-                                            </a>
+                                            <a href="manage-doctors.php">Administrar Doctores</a>
                                         </p>
                                     </div>
                                 </div>
@@ -119,30 +111,6 @@ if(empty($_SESSION)){
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="col-sm-4">
-                                <div class="panel panel-white no-radius text-center">
-                                    <div class="panel-body">
-                                        <span class="fa-stack fa-2x">
-                                            <i class="fa fa-square fa-stack-2x text-primary"></i>
-                                            <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                        <h2 class="StepTitle">Administrar Pacientes</h2>
-                                        <p class="links cl-effect-1">
-                                            <a href="manage-patient.php">
-                                                <!-- <php
-                                                $result = mysqli_query($con, "SELECT * FROM tblpatient");
-                                                $num_rows = mysqli_num_rows($result);
-                                                {
-                                                ?>
-                                                    Total Patients: <php echo htmlentities($num_rows); }
-                                                ?> -->
-                                            </a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
