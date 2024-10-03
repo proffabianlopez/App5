b7b2ebf673512c4937fc444b2a33ca1cbd346d40
Angel-Gomez0 <angelezequielgomez19@gmail.com>
2024-10-03 09:43:57 -0300

    redirecction

--- www/app/views/register.php
+++ www/app/views/register.php
@@ -89,7 +89,7 @@ if($conexion){
                 <button type="submit" class="btn btn-primary block full-width m-b">Registrarme</button>
 
                 <p class="text-muted text-center"><small>¿Ya tiene una cuenta?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="login.html">Login</a>
+                <a class="btn btn-sm btn-white btn-block" href="login.php">Login</a>
             </form>
             <p class="m-t"> <small>App5 derechos reservados &copy; 2024</small> </p>
         </div>
